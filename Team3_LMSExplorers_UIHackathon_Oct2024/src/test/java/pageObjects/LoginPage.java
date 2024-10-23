3d7a2c43b674ef83a6c4007f125ba5107575b07b
DebalinaChakrabarti <debalinachakrabarti@gmail.com>
2024-10-23 12:17:22 -0500

    fourth push

--- Team3_LMSExplorers_UIHackathon_Oct2024/src/test/java/pageObjects/LoginPage.java
+++ Team3_LMSExplorers_UIHackathon_Oct2024/src/test/java/pageObjects/LoginPage.java
@@ -21,5 +21,7 @@ public class LoginPage {
 		System.out.println("============baseURL================ "+Constants.baseURL);
 		driver.get(Constants.baseURL);
 	}
-
+	public void clickLoginButton() {
+	}
+	
 }
